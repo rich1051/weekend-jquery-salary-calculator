fd26f3874bf31f38c3f2a5ee9edebab90b2cc396
Will Richardson <rich1051@umn.edu>
2023-05-05 16:50:25 -0500

    separated button click from addToTable function.

--- myScript.js
+++ myScript.js
@@ -5,12 +5,32 @@ function onReady() {
     let employeeList = [];
     let totalMonthlyCost = 0;
 
-    $('#submitButton').on('click', function() {
-        let firstName = $('#firstName').val();
-		let lastName = $('#lastName').val();
-		let idNumber = $('#IDNumber').val();
-		let jobTitle = $('#titleName').val();
-		let annualSalary = $('#annualSalary').val();
-        console.log('click!');
-    });
+    $('#submitButton').on('click', addToTable)
+
+}
+
+function addToTable(event) {
+        let inputOne = $("#firstName").val();
+        let inputTwo = $("#lastName").val();
+        let inputThree = $('#IDNumber').val();
+        let inputFour = $('#titleName').val();
+        let inputFive = $('#annualSalary').val();
+        console.log(inputOne, inputTwo, inputThree, inputFour, inputFive);
+        
+        event.preventDefault();
+        $("#tableBody").append(`      
+      <tr>
+          <td>${inputOne}</td>
+          <td>${inputTwo}</td>
+          <td>${inputThree}</td>
+          <td>${inputFour}</td>
+          <td>${inputFive}</td>
+      </tr>
+        `)
+    // // $('#firstName').append();
+    // // let lastName = $('#lastName').val();
+    // // let idNumber = $('#IDNumber').val();
+    // // let jobTitle = $('#titleName').val();
+    // // let annualSalary = $('#annualSalary').val();
+    // console.log('click!');
 }
