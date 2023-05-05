5331d025a8573cc54217b70fa3582c4cfc0d9b2a
Will Richardson <rich1051@umn.edu>
2023-05-05 16:27:05 -0500

    began button  click functionality, made .html text more concise

--- myScript.js
+++ myScript.js
@@ -1,4 +1,16 @@
 $(document).ready(onReady);
 function onReady() {
     console.log('TEST: jQuery is connected!');
+
+    let employeeList = [];
+    let totalMonthlyCost = 0;
+
+    $('#submitButton').on('click', function() {
+        let firstName = $('#firstName').val();
+		let lastName = $('#lastName').val();
+		let idNumber = $('#IDNumber').val();
+		let jobTitle = $('#titleName').val();
+		let annualSalary = $('#annualSalary').val();
+        console.log('click!');
+    });
 }
